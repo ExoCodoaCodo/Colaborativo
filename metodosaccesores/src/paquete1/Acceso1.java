8bc8d054cfb139575ffcc41ec6b150d98cab82fa
alejandro <alejandro>
2017-09-07 14:48:19 -0300

    elimine los comentarios por defecto

---
+++ metodosaccesores/src/paquete1/Acceso1.java
@@ -0,0 +1,26 @@
+
+package paquete1;
+
+
+public class Acceso1 {
+    
+     int edad;
+     int altura=35;
+    
+    
+    
+    
+    public static void main(String [] args){
+    
+    Acceso1 obj1 = new Acceso1();
+    
+        obj1.edad=15;
+   System.out.println(obj1.edad);
+   
+   
+        System.out.println(obj1.altura);
+   
+    
+    }
+    
+}
