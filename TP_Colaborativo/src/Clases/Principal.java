2e8b560ddda20af1de712e14f2a6063b0e2be79b
alejandro <alejandrozapata73@gmail.com>
2017-11-02 15:10:07 -0300

    probando desde bash

--- TP_Colaborativo/src/Clases/Principal.java
+++ TP_Colaborativo/src/Clases/Principal.java
@@ -14,7 +14,7 @@ public class Principal {
     public static void main(String [] arg){
     
         System.out.println("hola mundo");
-    
+        System.out.println("hola mundo de alejandro 3");
     
     }
     
