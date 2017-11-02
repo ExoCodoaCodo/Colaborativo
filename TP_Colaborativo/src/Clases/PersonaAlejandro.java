6312e962c8f786ce5a890621398b77ad721de57d
alejandro <alejandro>
2017-11-02 14:40:18 -0300

    elimine los comentarios por defecto

---
+++ TP_Colaborativo/src/Clases/PersonaAlejandro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Español
+ */
+public class PersonaAlejandro {
+    
+    private int idusuario;
+    private String nombre;
+    private int edad;
+    private String domicilio;
+    private String dni;
+    private String telefono;
+    
+    
+    
+    
+}
