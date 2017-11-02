c0d184206dd56b5440ee4163c16e268a387a02eb
MarCalmon <marical2222@gmail.com>
2017-11-02 15:22:49 -0300

    PersonaMariana

---
+++ TP_Colaborativo/src/Clases/PersonaMariana.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author mariana
+ */
+public class PersonaMariana {
+    
+    private int idusuario;
+    private String nombre;
+    private int edad;
+    private String domicilio;
+    private String dni;
+    private String telefono;
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+    
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+    
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    public String getDni() {
+        return dni;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public String getTelefono() {
+        return telefono;
+    }
+    
+        
+}
